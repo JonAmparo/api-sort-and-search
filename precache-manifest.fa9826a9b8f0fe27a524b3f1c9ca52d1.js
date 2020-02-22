1faa46986a5942881c3a5c53de0d9fda12ed593e
JonAmparo <jonathanmamparo@gmail.com>
2020-02-22 16:24:28 -0500

    Updates

---
+++ precache-manifest.fa9826a9b8f0fe27a524b3f1c9ca52d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65acbd8cc6d50f362f0437df796cbddb",
+    "url": "/api-sort-and-search/index.html"
+  },
+  {
+    "revision": "da583bdeca51874418c5",
+    "url": "/api-sort-and-search/static/js/2.6990659b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/api-sort-and-search/static/js/2.6990659b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "563624f979a692a08713",
+    "url": "/api-sort-and-search/static/js/main.9c693263.chunk.js"
+  },
+  {
+    "revision": "9c2be993764a1aa7e7de",
+    "url": "/api-sort-and-search/static/js/runtime-main.0b904353.js"
+  }
+]);
